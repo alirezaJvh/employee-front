22a75ebddb520205c3b6c508eda5961dc28ae658
Alireza Javaheri <alireza.jvh98@gmail.com>
2022-10-02 20:29:35 +0330

    test ant design

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,61 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { loginEmployee } from '../../services'
-
 function Login({ dispatch }) {
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
+    // const [username, setUsername] = useState()
+    // const [password, setPassword] = useState()
+    // const [, forceUpdate] = useState({})
+    const [form] = Form.useForm()
 
-    const handleSubmit = async e => {
-        try {
-            e.preventDefault()
-            const payload = await loginEmployee({
-                username,
-                password,
-            })
-            if (payload.token) dispatch({ type: 'LOGIN', payload })
-        } catch (err) {
-            console.log(err)
-        }
-    }
+    return (
+        <Form form={form} name='login'>
+            <Form.Item 
+                name='username'
+                rules={[
+                    {
+                        required: true,
+                        message: 'Please input your username!'
+                    }
+                ]}
+            >
 
-    return(
-        <div className="login-wrapper">
-            <h1>Please Log In</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <p>Username</p>
-                    <input type="text" onChange={e => setUsername(e.target.value)}/>
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)}/>
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
+            </Form.Item>
+        </Form>
     )
+
+    // const handleSubmit = async e => {
+    //     try {
+    //         e.preventDefault()
+    //         const payload = await loginEmployee({
+    //             username,
+    //             password,
+    //         })
+    //         if (payload.token) dispatch({ type: 'LOGIN', payload })
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+    // }
+
+    // return(
+    //     <div className="login-wrapper">
+    //         <h1>Please Log In</h1>
+    //         <form onSubmit={handleSubmit}>
+    //             <label>
+    //                 <p>Username</p>
+    //                 <input type="text" onChange={e => setUsername(e.target.value)}/>
+    //             </label>
+    //             <label>
+    //                 <p>Password</p>
+    //                 <input type="password" onChange={e => setPassword(e.target.value)}/>
+    //             </label>
+    //             <div>
+    //                 <button type="submit">Submit</button>
+    //             </div>
+    //         </form>
+    //     </div>
+    // )
 }
 
 Login.propTypes = {
