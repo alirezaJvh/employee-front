836504a0fb1ccae395a0bc02fde599131cdab33d
Alireza Javaheri <alireza.jvh98@gmail.com>
2022-10-02 16:58:50 +0330

    getEmployee list in homepage

---
+++ src/hooks/useEmployees.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+import { getEmployees } from '../services';
+
+const useEmployees = () => {
+  const initEmployeeObj = {
+    currentPage: 0,
+    totalPages: 0,
+    totalItems: 0,
+    employees: []
+  }
+  const [resultObj, setEmployee] = useState(initEmployeeObj)
+  const [loading, setLoading] = useState(true)
+  const { headers } = useAuth()
+
+  useEffect(() => {
+    console.log('useEffect')
+    setLoading(true)
+    getEmployees({ headers }) 
+      .then(setEmployee)
+      .catch(console.error)
+      .finally(() => setLoading(false))
+  }, [])
+  return {...resultObj, loading, setEmployee}
+}
+
+export { useEmployees }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { memo } from 'react'
+import { useEmployees } from '../hooks/useEmployees'
 
 function Home() {
+  const { employees } = useEmployees()
+  {console.log('Home')}
+  {console.log(employees)}
   return (
     <div>
       this is home page
+      {employees.map(employee => <div key={employee.id}>{employee.username}</div>)}
     </div>
   )
 }
 
-export default Home
+export default memo(Home)
